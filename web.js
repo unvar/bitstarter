56726c90526b6953ffb2a11a3311b55cb3b57b5d
Varun Dodla <varun@dodla.com>
2013-07-12 04:45:28 +0000

    update to user toString instead of specifying the encoding

--- web.js
+++ web.js
@@ -4,10 +4,10 @@ var express = require('express'),
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', { encoding: 'utf8' }));
+  response.send(fs.readFileSync('index.html').toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
